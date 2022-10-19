ea829da005cad042a866f5434db41111efc7e863
CvijicZ <115740663+CvijicZ@users.noreply.github.com>
2022-10-19 10:43:15 +0200

    Header

---
+++ header.inc.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/prijava.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+            <nav class="nav">
+                <a href="#" class="nav-brand">CvijicGames.</a>
+            <ul class="nav-menu">
+                <li class="nav-item"> <a href="index.php" class="nav-link">Pocetna</a></li>
+                <li class="nav-item"> <a href="prijava.php" class="nav-link">Prijava</a></li>
+                <li class="nav-item"> <a href="registracija.php" class="nav-link">Registracija</a></li>
+            </ul>
+            <div class="hamburger">
+                <span class="bar"></span>
+                <span class="bar"></span>
+                <span class="bar"></span>
+            </div>
+        </nav>
+    </header> <br><br>
+
+    <script src="js/script.js"></script>
+</body>
+</html>
